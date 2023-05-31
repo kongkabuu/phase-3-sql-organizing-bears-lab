37dad4446aae431516cac9429c629540f7fb7cc2
paul kabuu <kongkabuu9@gmail.com>
2023-05-31 15:49:46 +0300

    assighnmnet complete

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(1, "Mr. chocolete",2,"M" ,"red","friendly",1);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(2, "Rowdy",2,"M" ,"blue","very angry",1);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(3, "Tabitha",2,"F" ,"blue","very angry",1);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(4, "Sergeant",2,"M" ,"blue","very angry",0);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(5, "Melissa",2,"F","blue","very angry",1);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(6, "Grich",2,"M" ,"blue","very angry",0);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(7, "Wendy",2,"F","blue","very angry",1);
+
+INSERT INTO bears(id , name,age ,sex,color,temperament,alive)
+VALUES(8, NULL,2,"M" ,"blue","very angry",0);
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,61 @@
 def selects_all_female_bears_return_name_and_age
   "Write your SQL query here"
+  <<-SQL
+  SELECT
+  bears.age
+  bears.name
+  FROM bears
+  WHERE sex="F"
+  SQL
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   "Write your SQL query here"
+  <<-SQL
+  SELECT
+  bears.name
+  FROM bears
+  ORDER BY ASC
+  SQL
+
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
   "Write your SQL query here"
+  <<-SQL
+  SELECT
+  bears.age
+  bears.name
+  FROM bears
+  ORDER BY bears.age
+  ASC
+  WHERE bears.alive = 1
+
+  SQL
 end
 
 def selects_oldest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  <<-SQL
+  SELECT
+  bears.age
+  bears.name
+  FROM bears
+ ORDER BY DESC
+ LIMIT 1
+
+  SQL
 end
 
 def select_youngest_bear_and_returns_name_and_age
   "Write your SQL query here"
+  <<-SQL
+  SELECT
+  bears.age
+  bears.name
+  FROM bears
+ ORDER BY ASC
+ LIMIT 1
+
+  SQL
 end
